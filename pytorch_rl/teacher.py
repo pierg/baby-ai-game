124150966d3bf05f2046cf706cbe4427cde58adf
simon555 <simon.sebbagh@free.fr>
2018-03-09 09:18:29 -0500

    fix reward

--- pytorch_rl/teacher.py
+++ pytorch_rl/teacher.py
@@ -96,11 +96,11 @@ class Teacher(Wrapper):
        #try to force the action toggle to be selected, abandonned
         if 3 in self.bestActions :
             if action in self.bestActions:
-                reward+=1
+                reward+=10
             else:
                 reward+=0
         else:
-            reward-=2
+            reward-=1
                 
         advice=self.generateAdvice()[1]
         
