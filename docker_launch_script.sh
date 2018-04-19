2f64222ed5588285b53d1a8e57dc96e0ef62d94b
Piergiuseppe <mallozzi@chalmers.se>
2018-04-19 17:48:38 +0200

    mod to docker_launch_script.sh

--- docker_launch_script.sh
+++ docker_launch_script.sh
@@ -5,7 +5,6 @@ configuration_file=${1:-"default.json"}
 if [ $# -eq 0 ]
   then
     echo "Using default configuration file"
-    exit 1
 fi
 
 
@@ -15,12 +14,19 @@ git pull
 cd ..
 cd gym-minigrid
 git pull
+cd ..
 
 # Run simulation
-cd baby-ai-game
+cd ./baby-ai-game
 
-PYTHONPATH=../gym-minigrid/gym_minigrid/:$PYTHONPATH
+source ./venv/bin/activate
+
+PYTHONPATH=../gym-minigrid/:$PYTHONPATH
+export PYTHONPATH
 PYTHONPATH=../gym-minigrid/gym_minigrid/envs/:$PYTHONPATH
 export PYTHONPATH
+PYTHONPATH=./:$PYTHONPATH
+export PYTHONPATH
+
 
 python3 ./pytorch_rl/main.py --env-name MiniGrid-UnsafeEnvironment-6x6-v0 --no-vis --num-processes 48 --algo a2c
