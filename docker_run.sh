8157ba002933cb8163613f24c2d63f453144ea21
Piergiuseppe <mallozzi@chalmers.se>
2018-04-20 14:47:44 +0200

    updated docker_run

--- docker_run.sh
+++ docker_run.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 if [ $# -eq 0 ]
-  then
-    docker run -it \
-	-p 5901:5901 \
-	-p 6901:6901 \
-	-p 8097:8097 \
-	baby-ai-game:safety_envelope \
-	bash
-fi
-else
-    if [ $1 == "hub" ]
     then
+        echo "running local image"
+        docker run -it \
+        -p 5901:5901 \
+        -p 6901:6901 \
+        -p 8097:8097 \
+        baby-ai-game:safety_envelope \
+        bash
+elif [ $1 = "hub" ]
+    then
+        echo "updating from dockerhub"
+        docker pull pmallozzi/baby-ai-game:safety_envelope
+        echo "running image"
         docker run -it \
 	    -p 5901:5901 \
 	    -p 6901:6901 \
 	    -p 8097:8097 \
 	    pmallozzi/baby-ai-game:safety_envelope \
 	    bash
-    fi
+else
+   echo "Unknown argument. Write 'hub'."
+fi
