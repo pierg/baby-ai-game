48ea182e3b4c7b994a3e0a0b67f6c6ffdb9338a7
Piergiuseppe <mallozzi@chalmers.se>
2018-04-20 13:36:01 +0200

    deleted entrypoint from dockerfile

---
+++ docker_entrypoint.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+configuration_file=${1:-"no_blocker.json"}
+
+if [ $# -eq 0 ]
+  then
+    echo "using default configuration file: $configuration_file"
+fi
+
+
+# Pull latest changes in the repositories
+echo "...updating repositories..."
+cd baby-ai-game
+git pull
+cd ..
+cd gym-minigrid
+git pull
+cd ..
+
+echo "...updating selected configuration file..."
+cd ./baby-ai-game/configurations
+yes | cp -rf $configuration_file "main.json"
+
+cd ..
+
+# Use virtual environment if exists
+if [ -d "venv" ]; then
+  echo "...activating python venv..."
+  source ./venv/bin/activate
+fi
+
+echo "...start training..."
+PYTHONPATH=../gym-minigrid/:../gym-minigrid/gym_minigrid/:./:$PYTHONPATH
+export PYTHONPATH
+python3 ./pytorch_rl/main.py
