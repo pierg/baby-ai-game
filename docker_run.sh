fdcf4654d62ada3b8c6b92d042b0995f38f1d927
Piergiuseppe <mallozzi@chalmers.se>
2018-04-20 14:59:45 +0200

    updated docker_run

--- docker_run.sh
+++ docker_run.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 if [ $# -eq 0 ]
     then
-        echo "running local image"
+        echo "running local image with default configuration"
         docker run -it \
         -p 5901:5901 \
         -p 6901:6901 \
         -p 8097:8097 \
-        baby-ai-game:safety_envelope \
-        bash
-elif [ $1 = "hub" ]
-    then
-        echo "updating from dockerhub"
-        docker pull pmallozzi/baby-ai-game:safety_envelope
-        echo "running image"
-        docker run -it \
-	    -p 5901:5901 \
-	    -p 6901:6901 \
-	    -p 8097:8097 \
-	    pmallozzi/baby-ai-game:safety_envelope \
-	    bash
+        baby-ai-game:safety_envelope
 else
-   echo "Unknown argument. Write 'hub'."
-fi
+   echo "running local image with configuration file: $1"
+        docker run -it \
+        -p 5901:5901 \
+        -p 6901:6901 \
+        -p 8097:8097 \
+        baby-ai-game:safety_envelope $1
