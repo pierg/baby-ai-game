2abd685d2771cc97593fcdcfe5fb3815a1549f22
Piergiuseppe <mallozzi@chalmers.se>
2018-04-23 15:25:27 +0200

    minors

--- pytorch_rl/envs.py
+++ pytorch_rl/envs.py
@@ -1,15 +1,8 @@
-import os
-import numpy
-import gym
-from gym import spaces
-import os
-from collections import namedtuple
-import json
 
 try:
     import gym_minigrid
     from gym_minigrid.wrappers import *
-    from gym_minigrid.envelopes import *
+    from gym_minigrid.envelopes import SafetyEnvelope
 
 except:
     pass
@@ -24,7 +17,8 @@ def make_env(env_id, seed, rank, log_dir, reset_on_catastrophe=False):
         env = gym.make(env_id)
         env.seed(seed + rank)
 
-        if config.blocker: env = SafetyEnvelope(env, reset_on_catastrophe=reset_on_catastrophe)
+        if config.blocker:
+            env = SafetyEnvelope(env, reset_on_catastrophe=reset_on_catastrophe)
 
         # Maxime: until RL code supports dict observations, squash observations into a flat vector
         if isinstance(env.observation_space, spaces.Dict):
