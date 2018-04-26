4710cbb17f8f95013bf27920fb6a9824597c147a
Philip Crockett <philipmayoo@gmail.com>
2018-04-26 12:18:52 +0200

    Always create the safety envelope env

--- pytorch_rl/envs.py
+++ pytorch_rl/envs.py
@@ -9,7 +9,7 @@ except:
 
 from helpers import config_grabber as cg
 
-def make_env(env_id, seed, rank, log_dir, reset_on_catastrophe=False):
+def make_env(env_id, seed, rank, log_dir):
 
     config = cg.Configuration.grab("blocker")
 
@@ -17,8 +17,7 @@ def make_env(env_id, seed, rank, log_dir, reset_on_catastrophe=False):
         env = gym.make(env_id)
         env.seed(seed + rank)
 
-        if config.blocker:
-            env = SafetyEnvelope(env, reset_on_catastrophe=reset_on_catastrophe)
+        env = SafetyEnvelope(env)
 
         # Maxime: until RL code supports dict observations, squash observations into a flat vector
         if isinstance(env.observation_space, spaces.Dict):
