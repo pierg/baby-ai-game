541ed8200cfff0544b9698cf5744c52cc0bc5079
haudegond <haudegond@ecole.ensicaen.fr>
2018-05-16 14:00:24 +0200

    Added config files for evaluations Modified plotResult.py to have it do the plot for all the csv files

--- plotResult.py
+++ plotResult.py
@@ -1,38 +1,49 @@
 import matplotlib.pyplot as plt
 import csv
+import glob
 
 """
 File used to create a graph from a csv file and the name of the columns that need to be used
 """
 
+def plotResult(columnNameX,columnNameY,fileName,resultFileName):
+    columnNameX = columnNameX
+    columnNameY = columnNameY
+    fileName = fileName
+    resultFileName = resultFileName
 
-# If you want to change the file or the columns used to create the graph do it here
-columnNameX = "N_updates"
-columnNameY = "Reward_mean"
-fileName = "evaluations/water_deadend_32x32.csv"
+    x = []
+    y = []
+    column1 = 0
+    column2 = 0
+
+    with open(fileName, 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        firstLine = True
+        for row in plots:
+            if firstLine:
+                for column in range(0, len(row)):
+                    if row[column] == columnNameX:
+                        column1 = column
+                    if row[column] == columnNameY:
+                        column2 = column
+                firstLine = False
+            else:
+                x.append(int(row[column1]))
+                y.append(float(row[column2]))
+    figure = plt.figure()
+    plt.plot(x, y)
+    plt.xlabel('numberOfStep')
+    plt.ylabel('reward_mean')
+    figure.savefig(resultFileName)
+
+def autoPlot(columnNameX,columnNameY):
+    for csvFile in glob.glob("evaluations/*.csv"):
+        str = csvFile
+        str = str.replace(".csv",".pdf")
+        str = str.replace("evaluations/","results/")
+        plotResult(columnNameX,columnNameY,csvFile,str)
 
 
-x = []
-y = []
-column1 = 0
-column2 = 0
 
-with open(fileName,'r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    firstLine = True
-    for row in plots:
-        if firstLine:
-            for column in range(0, len(row)):
-                if row[column] == columnNameX:
-                    column1 = column
-                if row[column] == columnNameY:
-                    column2 = column
-            firstLine = False
-        else:
-            x.append(int(row[column1]))
-            y.append(float(row[column2]))
-
-plt.plot(x,y)
-plt.xlabel('numberOfStep')
-plt.ylabel('reward_mean')
-plt.show()
+autoPlot("N_updates","Reward_mean")
