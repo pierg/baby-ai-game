47468a86ba95935b37cd08894d49488128106fd4
Philip Crockett <philipmayoo@gmail.com>
2018-05-17 22:46:32 +0200

    Added so the env generator updates the main.json file with the new random environment

--- env_generator.py
+++ env_generator.py
@@ -1,12 +1,12 @@
 import argparse
 import json
+from secrets import token_hex
 
 parser = argparse.ArgumentParser(description='Arguments for creating the environments and its configuration')
 parser.add_argument('--grid_size', type=int, required=True)
 parser.add_argument('--number_of_water_tiles', type=int, required=True)
 parser.add_argument('--max_block_size', type=int, required=True)
 
-from secrets import token_hex  # NOQA
 
 environment_path = "../gym-minigrid/gym_minigrid/envs/"
 configuration_path = "configurations/"
@@ -14,6 +14,9 @@ random_token = token_hex(4)
 
 
 def generate_environment(grid_size, nr_of_water_tiles, max_block_size):
+    # Creates a random environment python file, it currently only places water tiles randomly in the environment
+    # It uses the random_token as the seed for all its random functions, this to enable reproduction
+    # of environments. Using the random_token found in the end of file name.
     with open(environment_path + "randomenv{0}{1}.py".format(nr_of_water_tiles, random_token), 'w') as env:
         env.write("""
 from gym_minigrid.extendedminigrid import *
@@ -75,11 +78,14 @@ register(
 )
 """.format(grid_size, nr_of_water_tiles, random_token))
         env.close()
+    # Adds the import statement to __init__.py in the envs folder in gym_minigrid,
+    # otherwise the environment is unavailable to use.
     with open(environment_path + "__init__.py", 'a') as init_file:
         init_file.write("\n")
         init_file.write("from gym_minigrid.envs.randomenv{0}{1} import *".format(nr_of_water_tiles, random_token))
         init_file.close()
 
+    # Creates a json config file for the random environment
     with open(configuration_path + "randomEnv-{0}-{1}.json".format(nr_of_water_tiles, random_token), 'w') as config:
         config.write(json.dumps({
             "config_name": "firstEvalWaterRandomEnv",
@@ -114,6 +120,13 @@ register(
             }
         }))
         config.close()
+
+    # Updates the main.json file and set the env name to the randomly generated one
+    with open(configuration_path + "main.json", 'r+') as main_config:
+        data = json.load(main_config)
+        data["env_name"] = "MiniGrid-RandomEnv-{0}x{0}-v0".format(grid_size)
+        main_config.close()
+
     return "RandomEnv-{0}x{0}-Water-{1}-{2}".format(grid_size, nr_of_water_tiles, random_token)
 
 
