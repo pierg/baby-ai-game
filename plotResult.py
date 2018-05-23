a33e413320c5fde492cda68ec0203028782ce4bf
haudegond <haudegond@ecole.ensicaen.fr>
2018-05-23 15:14:34 +0200

    Add title (reward and monitors) into the plot

--- plotResult.py
+++ plotResult.py
@@ -5,13 +5,13 @@ from matplotlib.backends.backend_pdf import PdfPages
 import csv
 import glob
 from random import randint
+import configurations.config_grabber as cg
+import os
 """
 File used to create a graph from a csv file and the name of the columns that need to be used
 """
 
 def plotResult(tab,fileName,resultFileName):
-    fileName = fileName
-    resultFileName = resultFileName
     array = [[] for i in range(0,len(tab))]
     columnNumber = [0 for i in range(0,len(tab))]
 
@@ -29,53 +29,44 @@ def plotResult(tab,fileName,resultFileName):
                 for i in range(0,len(tab)):
                     array[i].append((float(row[columnNumber[i]])))
 
-    ymax = max(array[1])
-    xpos = array[1].index(ymax)
-    xmax = array[0][xpos]
-
     pp = PdfPages(resultFileName)
+    title = get_config_from_name(fileName)
     plt.figure()
-    plt.plot(array[0], array[1],'r',label='N Step AVG')
-    plt.annotate(ymax, xy=(xmax, ymax), xytext=(xmax, ymax+5))
-
-    ymax = max(array[2])
-    xpos = array[2].index(ymax)
-    xmax = array[0][xpos]
-
-    plt.plot(array[0], array[2],'b',label='N Goal Reached')
-    plt.annotate(ymax, xy=(xmax, ymax), xytext=(xmax, ymax+5))
-
-    plt.legend()
-    plt.xlabel('N Updates')
-    plt.savefig(pp,format='pdf')
-    plt.figure()
-
-    ymax = max(array[3])
-    xpos = array[3].index(ymax)
-    xmax = array[0][xpos]
-
-    plt.plot(array[0], array[3],'r',label='N Death')
-    plt.annotate(ymax, xy=(xmax, ymax), xytext=(xmax, ymax+5))
-
-    ymax = max(array[4])
-    xpos = array[4].index(ymax)
-    xmax = array[0][xpos]
-
-    plt.plot(array[0], array[4],'b',label='N Saved')
-    plt.annotate(ymax, xy=(xmax, ymax), xytext=(xmax, ymax+5))
-
-    ymax = max(array[5])
-    xpos = array[5].index(ymax)
-    xmax = array[0][xpos]
-
-    plt.plot(array[0], array[5],'g',label='N Episode')
-    plt.annotate(ymax, xy=(xmax, ymax), xytext=(xmax, ymax+5))
-
-
-    plt.legend()
-    plt.xlabel('N Updates')
-    plt.savefig(pp,format='pdf')
+    color = 'g'
+    for i in range(1,len(tab)):
+        ymax = max(array[i])
+        xpos = array[i].index(ymax)
+        xmax = array[0][xpos]
+        plt.plot(array[0], array[i],color,label=tab[i])
+        plt.annotate(ymax, xy=(xmax, ymax), xytext=(xmax,ymax-1 if ymax < 5 else ymax+5))
+        if i%2 == 0:
+            color = 'g'
+            plt.legend()
+            plt.title(title)
+            plt.xlabel('N Updates')
+            plt.savefig(pp,format='pdf')
+            plt.figure()
+        else:
+            color = 'b'
     pp.close()
+
+def get_config_from_name(file):
+    file = file.split(".csv")[0]
+    file = file.split("evaluations/")[1]
+    config = cg.Configuration.grab(file)
+    monitors = "Monitors : "
+    if hasattr(config.monitors, 'absence'):
+        for avoid_obj in config.monitors.absence.monitored:
+            if avoid_obj.active:
+                monitors += avoid_obj.name + " "
+    if hasattr(config.monitors, 'precedence'):
+        for precedence_obj in config.monitors.precedence.monitored:
+            if precedence_obj.active:
+                monitors += config.monitors + " "
+    rewards = "reward goal : {0} ".format(config.reward.goal)
+    rewards += "/ step : {0} ".format(config.reward.step)
+    rewards += "/ death : {0} ".format(config.reward.death)
+    return monitors + "\n" + rewards
 
 def autoPlot(tab):
     for csvFile in glob.glob("evaluations/*.csv"):
@@ -83,10 +74,9 @@ def autoPlot(tab):
         randomNumber = randint(0,999999)
         name = name.replace(".csv",str(randomNumber))
         name += str(".pdf")
-        print(name)
         name = name.replace("evaluations/","results/")
         plotResult(tab,csvFile,name)
 
 
-tab = ("N_updates","N_step_AVG","N_goal_reached","N_death","N_saved","N_Total_episodes")
+tab = ("N_updates","N_step_AVG","N_goal_reached","N_death","N_saved","Reward_mean","Reward_max")
 autoPlot(tab)
